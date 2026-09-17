e2a2a2bd85bfb8f2ce511446e934097af7db4e67
ACristobal <ajie.io@yahoo.com>
2026-09-16 22:00:34 -0400

    71 CORS

--- apps/api/src/infra/stacks/ApiStack.ts
+++ apps/api/src/infra/stacks/ApiStack.ts
@@ -2,8 +2,10 @@ import { Stack, StackProps } from "aws-cdk-lib";
 import {
   AuthorizationType,
   CognitoUserPoolsAuthorizer,
+  Cors,
   LambdaIntegration,
   MethodOptions,
+  ResourceOptions,
   RestApi,
 } from "aws-cdk-lib/aws-apigateway";
 import { IUserPool } from "aws-cdk-lib/aws-cognito";
@@ -37,7 +39,14 @@ export class ApiStack extends Stack {
       },
     };
 
-    const spacesResource = api.root.addResource("spaces");
+    const optionsWithCors: ResourceOptions = {
+      defaultCorsPreflightOptions: {
+        allowOrigins: Cors.ALL_ORIGINS,
+        allowMethods: Cors.ALL_METHODS,
+      },
+    };
+
+    const spacesResource = api.root.addResource("spaces", optionsWithCors);
     spacesResource.addMethod(
       "GET",
       props.spacesLambdaIntegration,
--- apps/api/src/services/spaces/handler.ts
+++ apps/api/src/services/spaces/handler.ts
@@ -9,6 +9,7 @@ import { getSpaces } from "./GetSpaces";
 import { updateSpace } from "./UpdateSpace";
 import { deleteSpace } from "./DeleteSpace";
 import { JsonError, MissingFieldError } from "../shared/Validator";
+import { addCorsHeader } from "../shared/Utils";
 
 const ddbClient = new DynamoDBClient({});
 
@@ -16,28 +17,33 @@ async function handler(
   event: APIGatewayProxyEvent,
   context: Context,
 ): Promise<APIGatewayProxyResult> {
-  let message: string;
+  let response: APIGatewayProxyResult = {
+    statusCode: 200,
+    body: "",
+  };
 
   try {
     switch (event.httpMethod) {
       case "GET":
-        const getResponse = await getSpaces(event, ddbClient);
-        console.log(getResponse);
-        return getResponse;
+        response = await getSpaces(event, ddbClient);
+        break;
       case "POST":
-        const postResponse = await postSpaces(event, ddbClient);
-        return postResponse;
+        response = await postSpaces(event, ddbClient);
+        break;
       case "PUT":
-        const putResponse = await updateSpace(event, ddbClient);
-        console.log(putResponse);
-        return putResponse;
+        response = await updateSpace(event, ddbClient);
+        break;
       case "DELETE":
-        const deleteResponse = await deleteSpace(event, ddbClient);
-        console.log(deleteResponse);
-        return deleteResponse;
+        response = await deleteSpace(event, ddbClient);
+        break;
       default:
-        return { statusCode: 405, body: JSON.stringify("Method not allowed") };
+        response = {
+          statusCode: 405,
+          body: JSON.stringify("Method not allowed"),
+        };
     }
+
+    addCorsHeader(response);
   } catch (error) {
     if (error instanceof MissingFieldError) {
       return {
@@ -56,6 +62,7 @@ async function handler(
       body: error instanceof Error ? error.message : String(error),
     };
   }
+  return response;
 }
 
 export { handler };
