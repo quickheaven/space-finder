7a9e99b12ca2f2fcd3966291a81597af9e35a234
ACristobal <ajie.io@yahoo.com>
2026-09-19 13:52:54 -0400

    96 CDK test matchers

--- apps/api/test/MonitorStack.test.ts
+++ apps/api/test/MonitorStack.test.ts
@@ -1,5 +1,5 @@
 import { App } from "aws-cdk-lib";
-import { Template } from "aws-cdk-lib/assertions";
+import { Capture, Template } from "aws-cdk-lib/assertions";
 import { MonitorStack } from "../src/infra/stacks/MonitorStack";
 
 describe("Monitor stack test suite", () => {
@@ -25,5 +25,18 @@ describe("Monitor stack test suite", () => {
       DisplayName: "AlarmTopic",
       TopicName: "AlarmTopic",
     });
+  });
+
+  test("Alarm actions", () => {
+    const alarmActionsCapture = new Capture();
+    monitorStackTemplate.hasResourceProperties("AWS::CloudWatch::Alarm", {
+      AlarmActions: alarmActionsCapture,
+    });
+
+    expect(alarmActionsCapture.asArray()).toEqual([
+      {
+        Ref: expect.stringMatching(/^AlarmTopic/),
+      },
+    ]);
   });
 });
