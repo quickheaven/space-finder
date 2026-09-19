b2d2e463bb02e94a0c08a71b576e667988d77c5f
ACristobal <ajie.io@yahoo.com>
2026-09-19 13:49:03 -0400

    95 CDK declarative test

---
+++ apps/api/test/MonitorStack.test.ts
@@ -0,0 +1,29 @@
+import { App } from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import { MonitorStack } from "../src/infra/stacks/MonitorStack";
+
+describe("Monitor stack test suite", () => {
+  let monitorStackTemplate: Template;
+
+  beforeAll(() => {
+    const testApp = new App({
+      outdir: "cdk.out",
+    });
+    const monitorStack = new MonitorStack(testApp, "MonitorStack");
+    monitorStackTemplate = Template.fromStack(monitorStack);
+  });
+
+  test("Lambda properties", () => {
+    monitorStackTemplate.hasResourceProperties("AWS::Lambda::Function", {
+      Handler: "index.handler",
+      Runtime: "nodejs18.x",
+    });
+  });
+
+  test("Sns topic properties", () => {
+    monitorStackTemplate.hasResourceProperties("AWS::SNS::Topic", {
+      DisplayName: "AlarmTopic",
+      TopicName: "AlarmTopic",
+    });
+  });
+});
